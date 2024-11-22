cdf3d41f31b329ee3fa7a82fb4ced771e70daa8a
Vulnsec-417 <diachoknazar11@gmail.com>
2024-11-22 00:43:04 +0000

    Update CrossFireX.py

--- CrossFireX.py
+++ CrossFireX.py
@@ -47,7 +47,7 @@ def check_for_updates(current_version):
 
 def download_and_replace_code():
     try:
-        code_url = "https://raw.githubusercontent.com/VulnsecHB/CrossFireX/main/xss2.py"
+        code_url = "https://raw.githubusercontent.com/VulnsecHB/CrossFireX/main/CrossFireX.py"
         response = requests.get(code_url, timeout=10)
         if response.status_code == 200:
             script_content = response.text
